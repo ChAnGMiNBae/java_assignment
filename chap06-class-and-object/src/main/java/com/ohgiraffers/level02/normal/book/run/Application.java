8e625163d8c7055993976e6df5f7b5b0d731561d
ChAnGMiNBae <qockdals752@gmail.com>
2025-09-10 16:00:11 +0900

    chap06-class-and-object 과제 제출

---
+++ chap06-class-and-object/src/main/java/com/ohgiraffers/level02/normal/book/run/Application.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.level02.normal.book.run;
+
+import com.ohgiraffers.level02.normal.book.model.dto.BookDTO;
+
+public class Application {
+    public static void main(String[] args) {
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.printInformation();
+
+        BookDTO bookDTO2 = new BookDTO("자바의 정석", "도우출판", "남궁성");
+        bookDTO2.printInformation();
+
+        BookDTO bookDTO3 = new BookDTO("홍길동전", "활빈당", "허균", 5000000, 0.5);
+        bookDTO3.printInformation();
+    }
+}
